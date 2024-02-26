965597f5227fb2c96c42a91306966def6bb47a47
Dean <accworkdean@gmail.com>
2024-02-26 22:51:52 +0700

    remove manual constructor anotation

--- src/main/java/com/example/assessment_backend_developer_xsis/service/MovieService.java
+++ src/main/java/com/example/assessment_backend_developer_xsis/service/MovieService.java
@@ -2,14 +2,12 @@ package com.example.assessment_backend_developer_xsis.service;
 
 import com.example.assessment_backend_developer_xsis.model.entity.Movie;
 import com.example.assessment_backend_developer_xsis.repository.MovieRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class MovieService {
 
     @Autowired
